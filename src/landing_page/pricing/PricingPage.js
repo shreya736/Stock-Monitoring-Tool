09e2244f5eca552080d2e4e7d143bae966b9bc1f
Shreya736 <shreyakadam1305@gmail.com>
2026-06-04 18:51:54 +0530

    Remove sensitive files

--- src/landing_page/pricing/PricingPage.js
+++ src/landing_page/pricing/PricingPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Brojkerage from './Brojkerage';
 import Hero from './Hero';
-//import Navbar from '../Navbar';
 import OpenAccount from '../OpenAccount';
 
 function PricingPage() {
--- src/landing_page/products/Universe.js
+++ src/landing_page/products/Universe.js
@@ -12,7 +12,7 @@ function Universe() {
             <p className='text-small text-muted'>Thematic investment platform</p>
         </div>
         <div className="col-4 p-3 mt-5">
-            <img src="media/images/streakLogo.png" />
+            <img src="media/images/streakLogo.png" style={{ width: "80px", height: "auto" }} />
             <p className='text-small text-muted'>Algo & strategy platform</p>
         </div>
         <div className="col-4 p-3 mt-5">
@@ -20,7 +20,7 @@ function Universe() {
             <p className='text-small text-muted'>Options trading platform</p>
         </div>
         <div className="col-4 p-3 mt-5">
-            <img src="media/images/zerodhaFundhouse.png" />
+            <img src="media/images/zerodhaFundhouse.png" style={{ width: "160px", height: "auto" }}/>
             <p className='text-small text-muted'>Asset management</p>
         </div>
         <div className="col-4 p-3 mt-5">
@@ -28,7 +28,7 @@ function Universe() {
             <p className='text-small text-muted'>Bonds trading platform</p>
         </div>
         <div className="col-4 p-3 mt-5">
-            <img src="media/images/dittoLogo.png" />
+            <img src="media/images/dittoLogo.png" style={{ width: "80px", height: "auto" }} />
             <p className='text-small text-muted'>Insurance</p>
         </div>
          <button className='p-2 btn btn-primary fs-5 mb-5' style={{ width: "20%", margin: "0 auto" }}>Signup Now</button>
